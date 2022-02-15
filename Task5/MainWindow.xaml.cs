29f1bc0ba135b7e8db76767da385f927663b64de
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-15 21:25:03 +0300

    feat: Task5

---
+++ Task5/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace Task5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            new Presenter(this);
+        }
+        private EventHandler myEvent = null;
+        public event EventHandler MyEvent
+        {
+            add { myEvent += value; }
+            remove { myEvent -= value; }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myEvent.Invoke(sender, e);
+        }
+    }
+}
